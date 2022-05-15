640f809d27a3c1275cfcf5d75b809ddf6fb8b966
yarman1 <gordienko045@gmail.com>
2022-05-15 17:41:17 +0300

    Changed naming of decimal reg expression

--- ip-manipulator.js
+++ ip-manipulator.js
@@ -2,18 +2,18 @@
 
 const REG_EXPESSIONS = {
   ipv4Hex: /^(0x)?[a-f0-9]{1,2}$/i,
-  ipv4Num: /^25[0-5]$|^2[0-4][0-9]$|^[01]?[0-9][0-9]$|^[0-9]$/,
+  ipv4Dec: /^25[0-5]$|^2[0-4][0-9]$|^[01]?[0-9][0-9]$|^[0-9]$/,
 };
 
 const ipv4 = {};
 
 ipv4.isValid = function(ip) {
   const parts = ip.split('.');
-  const ipv4Num = REG_EXPESSIONS.ipv4Num;
+  const ipv4Dec = REG_EXPESSIONS.ipv4Dec;
   const ipv4Hex = REG_EXPESSIONS.ipv4Hex;
   const res = [];
   for (const part of parts) {
-    if (!ipv4Num.test(part) && !ipv4Hex.test(part)) {
+    if (!ipv4Dec.test(part) && !ipv4Hex.test(part)) {
       return false;
     }
   }
