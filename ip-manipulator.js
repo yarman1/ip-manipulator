c9964baa5a0003c2338a6cff38005ef97734f279
yarman1 <gordienko045@gmail.com>
2022-05-15 18:12:57 +0300

    Added ipv4 length checking

--- ip-manipulator.js
+++ ip-manipulator.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const NUMBER_CONSTANTS = {
+  ipv4Length: 4,
+};
+
 const REG_EXPESSIONS = {
   ipv4Hex: /^(0x)?[a-f0-9]{1,2}$/i,
   ipv4Dec: /^25[0-5]$|^2[0-4][0-9]$|^[01]?[0-9][0-9]$|^[0-9]$/,
@@ -9,6 +13,10 @@ const ipv4 = {};
 
 ipv4.isValid = function(ip) {
   const parts = ip.split('.');
+  const { ipv4Length } = NUMBER_CONSTANTS;
+  if (parts.length > ipv4Length || parts.length < 1) {
+    return false;
+  }
   const ipv4Dec = REG_EXPESSIONS.ipv4Dec;
   const ipv4Hex = REG_EXPESSIONS.ipv4Hex;
   const res = [];
