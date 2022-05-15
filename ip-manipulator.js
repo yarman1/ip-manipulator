d37f7a115899fa486ba976d7a9417c042b024eca
yarman1 <gordienko045@gmail.com>
2022-05-15 17:17:28 +0300

    Added isValid function for ipv4 validation

--- ip-manipulator.js
+++ ip-manipulator.js
@@ -4,3 +4,18 @@ const REG_EXPESSIONS = {
   ipv4Hex: /^0x[a-f0-9]{2}$/i,
   ipv4Num: /^25[0-5]$|^2[0-4][0-9]$|^[01]?[0-9][0-9]$|^[0-9]$/,
 };
+
+const ipv4 = {};
+
+ipv4.isValid = function(ip) {
+  const parts = ip.split('.');
+  const ipv4Num = REG_EXPESSIONS.ipv4Num;
+  const ipv4Hex = REG_EXPESSIONS.ipv4Hex;
+  const res = [];
+  for (const part of parts) {
+    if (!ipv4Num.test(part) && !ipv4Hex.test(part)) {
+      return false;
+    }
+  }
+  return true;
+};
