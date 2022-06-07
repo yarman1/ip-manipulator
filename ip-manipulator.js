313283016b30b79be6b9555ac624e7cd082b0dd6
yarman1 <gordienko045@gmail.com>
2022-06-07 16:48:12 +0300

    Updated ipv6 isValid with correction of reg expressions

--- ip-manipulator.js
+++ ip-manipulator.js
@@ -2,15 +2,18 @@
 
 const NUMBER_CONSTANTS = {
   ipv4Length: 4,
+  ipv6Length: 8,
 };
 
 const V4_DEC_PART = '25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]|[0-9]';
+const V6_PART = '[0-9a-fA-F]';
 
 const IP_PARTS = {
   v4HexPart: '((0x)?[a-f0-9]{1,2}',
   v4DecFull: `((${V4_DEC_PART})\\.){3,3}(${V4_DEC_PART})`,
   v6Part: '[0-9a-fA-F]',
   v6ZoneId: '%[0-9a-z]{1,}',
+  v6Native: `(::)?(${V6_PART}{1,4}::?){0,}(${V6_PART}{1,4}){0,}:{0,2}`,
 };
 
 const IPV4_REG_EXPESSIONS = {
@@ -18,13 +21,11 @@ const IPV4_REG_EXPESSIONS = {
   v4Hex: `^${IP_PARTS.v4HexPart}\\.){3,3}${IP_PARTS.v4HexPart})$`,
 };
 
-// v6Native - must be reviewed
-
 const IPV6_REG_EXPRESSIONS = {
-  v6Native: `^(::)?(${IP_PARTS.v6Part}{1,4}::?){0,}(${IP_PARTS.v6Part}{1,4})){0,}(::)?$`,
-  v6LinkLocal: `^fe80:((${IP_PARTS.v6Part}{0,4}){0,4})${IP_PARTS.v6ZoneId}$`,
+  v6NativeFull: `^${IP_PARTS.v6Native}$`,
+  v6LinkLoc: `^fe80:((:${IP_PARTS.v6Part}){1,4}){0,4}|(:)${IP_PARTS.v6ZoneId}$`,
   v6Mapped: `^::ffff(:0{1,4}){0,1}:${IP_PARTS.v4DecFull}$`,
-  v6Embedded: `^(${IP_PARTS.v6Part}:){1,4}:${IP_PARTS.v4DecFull}$`,
+  v6Embedded: `^${IP_PARTS.v6Native}${IP_PARTS.v4DecFull}$`,
 };
 
 const ipv4 = {};
@@ -38,12 +39,29 @@ ipv4.isValid = function(ip) {
   return ipv4RegExp.test(ip);
 };
 
+ipv6.isEmbedded = function(ip) {
+  const lastPart = ip.split(':').pop();
+  return lastPart !== lastPart.split('.')[0];
+};
+
 ipv6.isValid = function(ip) {
-  const parts = ip.split(':');
-  const { ipv6 } = REG_EXPESSIONS;
-  for (const part of parts) {
-    if (!part) continue;
-    if (!ipv6.test(part)) return false;
+  const { ipv6Length } = NUMBER_CONSTANTS;
+  let ipLength = ip.split(':').length;
+  if (this.isEmbedded(ip)) {
+    ipLength += 1;
   }
-  return true;
+  if (ipLength > ipv6Length) return false;
+  if (ipLength < ipv6Length && ip.indexOf('::') === -1) {
+    return false;
+  }
+  if (ip.indexOf('::') !== ip.lastIndexOf('::')) {
+    return false;
+  }
+  const ipv6Full = [];
+  const keys = Object.keys(IPV6_REG_EXPRESSIONS);
+  for (const key of keys) {
+    ipv6Full.push(IPV6_REG_EXPRESSIONS[key]);
+  }
+  const ipv6RegExp = new RegExp(ipv6Full.join('|'), 'i');
+  return ipv6RegExp.test(ip);
 };
