4e1d5432471d318ebb0101e4a77c99256b00f9f9
yarman1 <gordienko045@gmail.com>
2022-06-07 11:36:15 +0300

    Added more ipv6 regexes

--- ip-manipulator.js
+++ ip-manipulator.js
@@ -4,10 +4,23 @@ const NUMBER_CONSTANTS = {
   ipv4Length: 4,
 };
 
-const REG_EXPESSIONS = {
-  ipv4Hex: '^((0x)?[a-f0-9]{1,2}\\.){3,3}((0x)?[a-f0-9]{1,2})$|',
-  ipv4DecPart: '25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]|[0-9]',
-  ipv6: /^[0-9a-f]{1,4}$/i,
+const IP_PARTS = {
+  v4HexPart: '((0x)?[a-f0-9]{1,2}',
+  v4DecPart: '25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]|[0-9]',
+  v4DecFull: `((${this.v4DecPart})\\.){3,3}(${this.v4DecPart})`,
+  v6Part: '[0-9a-fA-F]',
+  v6ZoneId: '%[0-9a-z]{1,}',
+};
+
+// v6Native - must be reviewed
+
+const IP_REG_EXPESSIONS = {
+  v4Dec: `^${IP_PARTS.v4DecFull}$`,
+  v4Hex: `^${IP_PARTS.v4HexPart}\\.){3,3}${IP_PARTS.v4HexPart})$`,
+  v6Native: `^(::)?(${IP_PARTS.v6Part}{1,4}::?){0,}(${IP_PARTS.v6Part}{1,4})){0,}(::)?$`,
+  v6LinkLocal: `^fe80:((${IP_PARTS.v6Part}{0,4}){0,4})${IP_PARTS.v6ZoneId}$`,
+  v6Mapped: `^::ffff(:0{1,4}){0,1}:${IP_PARTS.v4DecFull}$`,
+  v6Embed: `^(${IP_PARTS.v6Part}:){1,4}:${IP_PARTS.v4DecFull}$`,
 };
 
 const ipv4 = {};
@@ -17,7 +30,7 @@ ipv4.isValid = function(ip) {
   if (!ip) return false;
   const { ipv4DecPart, ipv4Hex } = REG_EXPESSIONS;
   const ipv4Dec = '^((' + ipv4DecPart + ')\\.){3,3}(' + ipv4DecPart + ')$';
-  const regExp = new RegExp(ipv4Hex + ipv4Dec, 'i');
+  const regExp = new RegExp(ipv4Hex + '|' + ipv4Dec, 'i');
   return regExp.test(ip);
 };
 
