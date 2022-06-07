c2db1613b375ec18035fafe959e32e44d9a4ee8a
yarman1 <gordienko045@gmail.com>
2022-06-07 12:24:33 +0300

    Divided ipv4 and ipv6 reg expes

--- ip-manipulator.js
+++ ip-manipulator.js
@@ -13,15 +13,18 @@ const IP_PARTS = {
   v6ZoneId: '%[0-9a-z]{1,}',
 };
 
-// v6Native - must be reviewed
-
-const IP_REG_EXPESSIONS = {
+const IPV4_REG_EXPESSIONS = {
   v4Dec: `^${IP_PARTS.v4DecFull}$`,
   v4Hex: `^${IP_PARTS.v4HexPart}\\.){3,3}${IP_PARTS.v4HexPart})$`,
+};
+
+// v6Native - must be reviewed
+
+const IPV6_REG_EXPRESSIONS = {
   v6Native: `^(::)?(${IP_PARTS.v6Part}{1,4}::?){0,}(${IP_PARTS.v6Part}{1,4})){0,}(::)?$`,
   v6LinkLocal: `^fe80:((${IP_PARTS.v6Part}{0,4}){0,4})${IP_PARTS.v6ZoneId}$`,
   v6Mapped: `^::ffff(:0{1,4}){0,1}:${IP_PARTS.v4DecFull}$`,
-  v6Embed: `^(${IP_PARTS.v6Part}:){1,4}:${IP_PARTS.v4DecFull}$`,
+  v6Embedded: `^(${IP_PARTS.v6Part}:){1,4}:${IP_PARTS.v4DecFull}$`,
 };
 
 const ipv4 = {};
@@ -29,7 +32,7 @@ const ipv6 = {};
 
 ipv4.isValid = function(ip) {
   if (!ip) return false;
-  const { v4Dec, v4Hex } = IP_REG_EXPESSIONS;
+  const { v4Dec, v4Hex } = IPV4_REG_EXPESSIONS;
   const ipv4Full = v4Dec + '|' + v4Hex;
   const ipv4RegExp = new RegExp(ipv4Full, 'i');
   return ipv4RegExp.test(ip);
