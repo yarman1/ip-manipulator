10227c35c177bac01b89cb1225a50d5d23e5df6c
yarman1 <gordienko045@gmail.com>
2022-06-11 15:52:31 +0300

    Added data for tests

---
+++ test/ip-manipulator.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { ipv4, ipv6 } = require('../ip-manipulator');
+const assert = require('assert').strict;
+
+// Tests of validation system.
+// It can validate: decimal and hexadecimal(isn't case sensitive) IPv4 addresses
+// IPv6: native, link local, mapped, IPv4 embedded variants
+{
+  const validationIpv4Tests = [
+    ['192.168.0.1', true, 'Valid decimal IPv4 address'],
+    ['256.255.255.255', false, 'Invalid decimal IPv4 address'],
+    ['c0.A8.0.1', true, 'Valid hexadecimal IPv4 address'],
+    ['0xc0.0xA8.0x0.0x1', true, 'Another example of valid hex IPv4 address'],
+    ['ac.10.8e.fff', false, 'Invalid hexadecimal IPv4 address'],
+  ];
+}
+
+{
+  const validationIpv6Tests = [
+    ['2001.0db8::8A2e:07a0:765d', true, 'Valid IPv6 native address'],
+    ['fd3f::c126:9e70:532f::', false, 'Invalid IPv6 native address'],
+    ['fddf:ebc3:cfcff::65ce:f32a', false, 'Another bad IPv6 native address'],
+    ['fe80::ce80:ff88%eth2', true, 'Link local IPv6 address'],
+    ['::ffff:192.168.0.1', true, 'Mapped IPv6 address'],
+    ['64:ff96:1:a345:c70:2cfa:192.168.0.1', true, 'IPv4 embedded IPv6 address'],
+  ];
+}
